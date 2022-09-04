0de8c4d4e5da5c2a87d805a26fc0cfb1fba1ec51
wy <thelmg@163.com>
2022-09-04 10:26:20 +0800

    chore: fix package config

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: [require('prettier-plugin-packagejson'), require('prettier-plugin-tailwindcss')],
+  plugins: [require('prettier-plugin-tailwindcss')],
   printWidth: 100,
   semi: true,
   vueIndentScriptAndStyle: true,
@@ -9,4 +9,8 @@ module.exports = {
   htmlWhitespaceSensitivity: 'strict',
   endOfLine: 'auto',
   tabWidth: 2,
+  // 是否使用tab进行缩进，默认为false，表示用空格进行缩减
+  useTabs: false,
+  // 对象大括号直接是否有空格，默认为true，效果：{ foo: bar }
+  bracketSpacing: true,
 };
--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,16 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
+  readonly VITE_APP_DEV: string;
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_APP_ENV: string;
+  readonly VITE_APP_SSO_KEY: string;
+  readonly VITE_APP_CORE_URL: string;
+  readonly VITE_APP_SSO_DOMAIN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
